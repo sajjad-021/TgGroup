cb35dd7769445b6e6568530f707dc61f0a18c146
sajjad_021 <sajjad-021@users.noreply.github.com>
2017-06-12 23:00:17 +0430

    Create launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
-while true; do
-  ./tg -s bot.lua
-done
+
+THIS_DIR=$(cd $(dirname $0); pwd)
+cd $THIS_DIR
+
+COUNTER=0
+
+  while [ $COUNTER -lt 5 ]; do
+
+       tmux kill-session -t script
+
+           tmux new-session -s script "./telegram-cli -s bot.lua"
+
+        tmux detach -s script
+        
+     sleep 1
+
+  done
